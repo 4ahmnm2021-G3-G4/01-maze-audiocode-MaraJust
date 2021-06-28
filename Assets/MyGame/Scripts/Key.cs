4b8b9021860ece07140a934373ced00e818d444f
Mara Just <marajust@mjust03-MBP.fritz.box>
2021-06-28 23:19:14 +0200

    create scripts for end

---
+++ Assets/MyGame/Scripts/Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public Transform newTransform;
+    public GameObject efeuwand;
+    public AudioSource doorOpener;
+    public float speed = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            openDoor();
+            doorOpener.Play();
+        }
+    }
+
+    void openDoor()
+    {
+        efeuwand.transform.position = Vector3.MoveTowards(efeuwand.transform.position, newTransform.position, speed);
+    }
+}
