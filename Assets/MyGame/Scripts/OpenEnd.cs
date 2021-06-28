6fd45946b1229d7695b88b2b87078edbb51809f8
Mara Just <marajust@mjust03-MBP.fritz.box>
2021-06-28 06:27:19 +0200

    add more assets and a little bit light; modify script

---
+++ Assets/MyGame/Scripts/OpenEnd.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenEnd : MonoBehaviour
+{
+    public int picnicItemCount = 0;
+
+    public AudioSource doorOpener;
+    public GameObject efeuwand;
+    
+    // Udate is called once per frame
+    void Update()
+    {
+        if (picnicItemCount == 4)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "picknickobject")
+        {
+            picnicItemCount += picnicItemCount;
+            Debug.Log(picnicItemCount);
+        }
+    }
+}
